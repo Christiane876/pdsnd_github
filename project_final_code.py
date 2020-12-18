dbafdbef497d7d2ed10c213a45f4767ab52e9d80
Christiane Molz <gesianne@gmx.de>
2020-12-18 20:05:00 +0100

    fix: Add reference in main python function

--- project_final_code.py
+++ project_final_code.py
@@ -207,7 +207,7 @@ def main():
     while True:
         city, month, day = get_filters()
         df = load_data(city, month, day)
-        time_stats(df)
+        time_stats(df,month,days_of_week)
         station_stats(df)
         trip_duration_stats(df)
         user_stats(df)
